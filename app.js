143c407d108dd9777a3c6a114aff33673eb42b76
Jose Luis Mañon <joseluismanonrosario@gmail.com>
2024-03-22 12:02:31 -0400

    Agrega las funciones de inicializar, blink, mensajes a la aplicacones y algunos areglos a los estilos

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let userInput=document.getElementById("input-field");
 let checkBtn=document.getElementById("check-button");
 let messageBox=document.getElementById("messages");
+let blinkingMessage=document.getElementsByClassName("blink");
+let subtitleMessage=document.getElementById("welcomeSubtitle");
 
 checkBtn.addEventListener('click',guessChecker);
 
@@ -13,8 +15,13 @@ userInput.addEventListener('keydown',(e)=>{
 function guessChecker(){
     let userGuess=userInput.value;
     let finalMessage=(`Your Guess is ${userGuess}`);
-    messageBox.innerText=finalMessage;
+    subtitleMessage.style.opacity=0;
+    setMessages(finalMessage);
     clearUserInput();
+}
+
+function setMessages(message){
+    messageBox.innerText=message;
 }
 
 function clearUserInput(){
@@ -22,3 +29,12 @@ function clearUserInput(){
 }
 
 
+setInterval(function() {
+    blinkingMessage[0].style.opacity = (blinkingMessage[0].style.opacity == 0 ? 1 : 0);
+}, 500);
+
+function initGame(){
+    setMessages("");
+}
+
+initGame();
